a2d78fb142a681ec250bb4f690ed49e5be507849
Dizzrt <dizzrt.s@qq.com>
2023-12-06 12:37:18 +0800

    frontend init

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 120,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  bracketSameLine: true,
+  vueIndentScriptAndStyle: false,
+  endOfLine: 'lf',
+  singleQuote: true,
+  trailingComma: 'es5',
+  htmlWhitespaceSensitivity: 'ignore',
+  singleAttributePerLine: false,
+  proseWrap: 'never',
+};
